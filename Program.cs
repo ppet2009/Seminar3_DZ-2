95293f03ad24e0576d93926380218529d1be65a8
Peter <peter_8787@outlook.com>
2022-10-28 19:36:31 +0300

    решена Задача2

--- Program.cs
+++ Program.cs
@@ -22,3 +22,40 @@ int result = SecondNumber(num);
 Console.WriteLine($"Вторая цифра введеного трехзначного числа = {result} ");
 
 */
+
+/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+    
+    if (amount < 3 || amount > 3)
+        {
+            Console.WriteLine("Вы ввели не трехзначное число");
+        }
+    else
+        {
+            Console.WriteLine(InCenter(number));
+        }
+int ReadInt(string message)
+
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a % 10));
+    return result;
+}
+
+
